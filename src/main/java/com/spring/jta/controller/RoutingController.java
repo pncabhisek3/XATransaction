f36339de598212869d082bf991cfa51a015e0985
abhisekm <abhisekm@cisco.com>
2020-07-31 19:11:44 +0530

    routing Controller for forwarding to baseHref of angular 10

---
+++ src/main/java/com/spring/jta/controller/RoutingController.java
@@ -0,0 +1,14 @@
+package com.spring.jta.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RoutingController {
+
+	@RequestMapping(value = "/**/{[path:[^\\.]*}")
+	public String redirect() {
+		// Forward to home page so that angular routing is preserved.
+		return "forward:/";
+	}
+}
